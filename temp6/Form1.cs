3c39b749bec01cb15361659f9e37a45683ed87c8
yinondotan <36883618+yinondotan@users.noreply.github.com>
2018-03-20 12:42:04 +0200

    Add project files.

---
+++ temp6/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace temp6
+{
+    public partial class Form1 : Form
+    {
+        string[] dates;
+        public Form1()
+        {
+            InitializeComponent();
+            //comboBox1.AutoScrollOffset=
+            // dates = new string[] {"1","2",
+
+            //"3",
+            //"5"};
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var biggerThan2 =
+                  from n in dates
+                  where int.Parse(n) > 2
+                  select n;
+
+            foreach (var item in biggerThan2)
+            {
+                MessageBox.Show(item+"");
+            }
+        }
+    }
+}
